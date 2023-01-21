1b1e40d5913d51178840499be5af5f6381552f75
TDF-PL-008 <112808404+TDF-PL-008@users.noreply.github.com>
2023-01-21 10:04:37 +0100

    Remove Java

--- java/src/main/java/wot/tak/connection/TAKServerConnector.java
+++
@@ -1,9 +0,0 @@
-package wot.tak.connection;
-
-import java.io.File;
-
-public interface TAKServerConnector {
-    public void sendFile(File file) throws Exception;
-    public void connect() throws Exception;
-    public void close() throws Exception;
-}
